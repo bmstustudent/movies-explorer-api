c9e4935c3aa8842c417d5b4ff1df6ab4573e1a3e
Shults Pavel <bmstustudent@yandex.ru>
2021-02-17 16:19:48 +0300

    fix: добавил коментарий из тренажёра

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,9 +1,11 @@
 const { SERVER_ERROR_MESSAGE } = require('../utils/utils');
 
 const errorHandler = (err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({
+    // проверяем статус и выставляем сообщение в зависимости от него
     message: statusCode === 500 ? SERVER_ERROR_MESSAGE : message,
   });
   next();
